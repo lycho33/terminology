1b5c493d0e236680fc795c9829be582c94d1b30e
lycho33 <cho.lyds@gmail.com>
2025-06-07 16:51:26 -0400

    remove prisma, kysely, postgres, and typescript

--- src/graphdb/main.py
+++ src/graphdb/main.py
@@ -12,10 +12,31 @@ NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 # Create a driver instance
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
+def create_with_existing_node(type, value, relationship, existingNode, existingNodeType):
+    result = driver.execute_query(f"""
+        MATCH (a:`{existingNodeType}` {{name: $name}})
+        CREATE (b:`{type}` {{name: $newValue}})
+        CREATE (a)-[:{relationship}]->(b)
+        RETURN a.name AS name
+        """,
+        name=existingNode,
+        newValue=value,
+        database_="neo4j"  # Optional if you use single database
+    )
+
+    # Extract data from result
+    records = list(result.records) if hasattr(result, "records") else result
+    if records:
+        for record in records:
+            print("Found name:", record["name"])
+        print("Query summary:", result.summary)
+    else:
+        print("No matching record found.")
+
 def create():
     summary = driver.execute_query("""
         CREATE (a:Term {name: $term})
-        CREATE (b:Definition {name: $definition})
+        CREATE (b:Definition {definition: $definition})
         CREATE (a)-[:DEFINED_AS]->(b)
         """,
         term="Docker", definition="A tool that makes it easier to create, share, and run applications",
@@ -25,12 +46,13 @@ def create():
         nodes_created=summary.counters.nodes_created,
         time=summary.result_available_after
     ))
-def delete(value):
+
+def delete(label, value):
     # This does not delete _only_ p, but also all its relationships!
-    records, summary, keys = driver.execute_query("""
-        MATCH (p:Term {name: $name})
-        DETACH DELETE p
-        """, name={value},
+    records, summary, keys = driver.execute_query(f"""
+        MATCH (a:`{label}` {{name: $name}})
+        DETACH DELETE a
+        """, name=value,
         database_="neo4j",
     )
     print(f"Query counters: {summary.counters}. {records} {keys}")
@@ -38,8 +60,9 @@ def delete(value):
 # Execute query
 def main():
     print('🏁 Start Query 🏁')
-    create()
-    # delete()
+    create_with_existing_node("Term", "DockerFile", "RELATED_TO", "Docker", "Term")
+    # create()
+    # delete("Term", "DockerFile")
     print('🏁 Finished 🏁')
 
 if __name__ == "__main__":
